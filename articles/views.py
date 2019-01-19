963902f59108239364b4858de1f55e1e3a621bea
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-01-19 14:03:44 -0500

    Configure URL for home page

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Article
+
 
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/home.html'
